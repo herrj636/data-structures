91b8f050a8e72602a6d303ccac2ad13c23bbdebd
herrj636 <30585017+herrj636@users.noreply.github.com>
2018-09-02 20:54:00 -0400

    Adding data files and code documentation

--- assignment-1/week1.js
+++ assignment-1/week1.js
@@ -3,13 +3,18 @@ var request = require('request');
 var fs = require('fs');
 var fileNum = [01, 02, 03, 04, 05, 06, 07, 08, 09, 10];
 
+//Command running the output function for every item in the fileNum array.
 fileNum.forEach(output);
 
+//Function running the request through Node.js.
 function output(items, index, array) {
     var alpha = "https://parsons.nyc/aa/m";
     var bravo = ".html";
     
+    //For all items in the array where the index is less than 9 run the following code.
     if (index < 9) {
+        
+        //add a '0' at the end of alpha and run the request for each item.
         alpha = alpha.concat('0');
         newURL = alpha.concat(items,bravo)
         
@@ -22,7 +27,10 @@ function output(items, index, array) {
     
     })}
     
+    // if the index of the item is larger than 9, run the following code.
     else {
+    
+        //Note: NOT adding a '0' to the end of alpha.
         var newURL = alpha.concat(items,bravo)
         request(String(newURL), function(error, response, body){
             if (!error && response.statusCode == 200) {
