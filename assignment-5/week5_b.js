8ed1fe135ccc3f479345eac10c4a8cec7e62a6dd
herrj636 <30585017+herrj636@users.noreply.github.com>
2018-10-12 14:50:00 -0400

    HW Push

--- assignment-5/week5_b.js
+++ assignment-5/week5_b.js
@@ -1,4 +1,7 @@
+
 var AWS = require('aws-sdk');
+var async = require('async');
+
 AWS.config = new AWS.Config();
 AWS.config.accessKeyId = process.env.AWS_ID;
 AWS.config.secretAccessKey = process.env.AWS_KEY;
@@ -6,11 +9,20 @@ AWS.config.region = "us-east-1";
 
 var dynamodb = new AWS.DynamoDB();
 
-var params = {};
-params.Item = diaryEntries[0]; 
-params.TableName = "deardiary";
+var diaryEntries = getData();
 
-dynamodb.putItem(params, function (err, data) {
-  if (err) console.log(err, err.stack); // an error occurred
-  else     console.log(data);           // successful response
-});
+async.eachSeries(diaryEntries, function(value, callback) {
+        var params = {};
+        params.Item = value; 
+        params.TableName = "deardiary";
+        
+        dynamodb.putItem(params, function (err, data) {
+          if (err) console.log(err, err.stack); // an error occurred
+          else     console.log(data);           // successful response
+        });
+    setTimeout(callback, 1000); 
+}); 
+
+function getData() {
+ return  [ { 'pk': { 'N': '0' }, 'date': { 'S': 'Thu Oct 11 2018' }, 'time': { 'N': '1941' }, 'price': { 'N': '4' }, 'drink': { 'S': 'beer' }, 'liquor': { 'S': 'beer' }, 'drinkName': { 'S': 'Sam Adams' }, { 'pk': { 'N': '1' }, 'date': { 'S': 'Thu Oct 11 2018' }, 'time': { 'N': '2017' }, 'price': { 'N': '4' }, 'drink': { 'S': 'beer' }, 'liquor': { 'S': 'beer' }, 'drinkName': { 'S': 'Sam Adams' }, { 'pk': { 'N': '2' }, 'date': { 'S': 'Thu Oct 11 2018' }, 'time': { 'N': '2103' }, 'price': { 'N': '4' }, 'drink': { 'S': 'beer' }, 'liquor': {'S': 'beer' }, 'drinkName': { 'S': 'Bud Light' } ];
+}
