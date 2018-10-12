c5d6eb22132baf48c2e260f26c35820da223de23
herrj636 <30585017+herrj636@users.noreply.github.com>
2018-10-12 14:27:59 -0400

    HW

--- assignment-5/week5_a.js
+++ assignment-5/week5_a.js
@@ -1,23 +1,24 @@
 var diaryEntries = [];
 
 class DiaryEntry {
-  constructor(primaryKey, date, entry, happy, iate) {
+  constructor(primaryKey, date, time, value, entry, liquor) {
     this.pk = {};
     this.pk.N = primaryKey.toString();
     this.date = {}; 
     this.date.S = new Date(date).toDateString();
+    this.time = {}; 
+    this.time.N = new String(time).toDateString();
+    this.value = {}; 
+    this.value.N = new String(time).toDateString();
     this.drink = {};
     this.drink.S = entry;
     this.liquor = {};
-    this.liquor.S = entry;
-    this.happy = {};
-    this.happy.BOOL = happy;
+    this.liquor.S = liquor;
   }
 }
 
-diaryEntries.push(new DiaryEntry(0, 'March 10, 1976', "I was born!", true, ["baby food", "baby formula"]));
-diaryEntries.push(new DiaryEntry(1, 'October 31, 2015', "I piloted my first solo flight!", true, ["pancakes"]));
-diaryEntries.push(new DiaryEntry(2, 8675309, "867-5309?", false));
-diaryEntries.push(new DiaryEntry(3, 'October 8, 2018', "I taught my favorite students.", true, ["peas", "carrots"]));
+diaryEntries.push(new DiaryEntry(0, 'October 12, 2018', "I was born!", true, ["baby food", "baby formula"]));
+diaryEntries.push(new DiaryEntry(1, 'October 12, 2018', "I piloted my first solo flight!", true, ["pancakes"]));
+diaryEntries.push(new DiaryEntry(2, 'October 12, 2018', false));
 
 console.log(diaryEntries);
