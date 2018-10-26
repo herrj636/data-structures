3b8b9d3980d66f229522dd80cdf36763bdf91bb5
herrj636 <30585017+herrj636@users.noreply.github.com>
2018-10-26 15:00:50 -0400

    Upadtes to hw 6

--- assignment-6/sql-query/week-6.js
+++ assignment-6/sql-query/week-6.js
@@ -5,7 +5,7 @@ const cTable = require('console.table');
 var db_credentials = new Object();
 db_credentials.user = 'herrj636';
 db_credentials.host = 'db.cumu7khhlm3w.us-east-2.rds.amazonaws.com';
-db_credentials.database = 'aaDatabase';
+db_credentials.database = 'aaDatabaseTwo';
 db_credentials.password = process.env.AWSRDS_PW;
 db_credentials.port = 5432;
 
@@ -14,7 +14,7 @@ const client = new Client(db_credentials);
 client.connect();
 
 // Sample SQL statement to query meetings on Monday that start on or after 7:00pm: 
-var thisQuery = "SELECT mtgday, mtgtime, mtglocation, mtgaddress, mtgtypes FROM aadata WHERE mtgday = 'Monday' and mtghour >= 7;";
+var thisQuery = "SELECT oldaddress, lat, lon, wheelchair FROM aadata WHERE wheelchair = '0'";
 
 client.query(thisQuery, (err, res) => {
     if (err) {throw err}
