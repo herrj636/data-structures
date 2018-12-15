a345f67f925267068e7dd358fecc87104e4694cf
Juan Herrera <herrj636@newschool.edu>
2018-12-15 16:34:22 -0500

    updates to hw 4

--- assignment-4/week4_createTable.js
+++ assignment-4/week4_createTable.js
@@ -9,25 +9,21 @@ db_credentials.database = 'aaDatabase';
 db_credentials.password = process.env.AWSRDS_PW;
 db_credentials.port = 5432;
 
+// var addressesForDb = [{"oldaddress":"20 Cardinal Hayes Place","new address":"20 CARDINAL HAYES PL New York NY ","lat":"40.7132514","lon":"-74.002398","adrMeta":["Rectory Basement","Between Duane and Pearl St"],"title":"St Andrews Church","wheelchair":false,"meetings":["Thursdays","7:00 AM","8:00 AM","OD"]},{"oldaddress":"20 Cardinal Hayes Place","new address":"20 CARDINAL HAYES PL New York NY ","lat":"40.7132514","lon":"-74.002398","adrMeta":["Rectory Basement","Between Duane and Pearl St."],"title":"St Andrews Church","wheelchair":false,"meetings":["Tuesdays","7:00 AM","8:00 AM","Op"]},{"oldaddress":"20 Cardinal Hayes Place","new address":"20 CARDINAL HAYES PL New York NY ","lat":"40.7132514","lon":"-74.002398","adrMeta":["Enter through driveway behind Church."],"title":"St. Andrew's Church","wheelchair":false,"meetings":["Mondays","12:15 PM","1:15 PM","OD"]},{"oldaddress":"20 Cardinal Hayes Place","new address":"20 CARDINAL HAYES PL New York NY ","lat":"40.7132514","lon":"-74.002398","adrMeta":["Enter through driveway behind Church."],"title":"St. Andrew's Church","wheelchair":false,"meetings":["Wednesdays","12:15 PM","1:15 PM","Op"]},{"oldaddress":"20 Cardinal Hayes Place","new address":"20 CARDINAL HAYES PL New York NY ","lat":"40.7132514","lon":"-74.002398","adrMeta":["Enter through driveway behind Church."],"title":"St. Andrew's Church","wheelchair":false,"meetings":["Thursdays","12:15 PM","1:15 PM","S"]},{"oldaddress":"20 Cardinal Hayes Place","new address":"20 CARDINAL HAYES PL New York NY ","lat":"40.7132514","lon":"-74.002398","adrMeta":["Enter through driveway behind Church."],"title":"St. Andrew's Church","wheelchair":false,"meetings":["Fridays","12:15 PM","1:15 PM","B"]}];
+var addressesForDb = JSON.parse(fs.readFileSync('data/finalCompiledData.json'));
+console.log(addressesForDb)
 
+async.eachSeries(addressesForDb, function(value, callback) {
+    const client = new Client(db_credentials);
+    client.connect();
+    var cleanTitle = value.title.replace(/\'/g,"");
+    console.log(cleanTitle)
 
-// Connect to the AWS RDS Postgres database
-const client = new Client(db_credentials);
-client.connect();
-
-// Sample SQL statement to create a table: 
-var thisQuery = "CREATE TABLE aalocations (address varchar(200), lat double precision, long double precision);";
-// Sample SQL statement to delete a table: 
-// var thisQuery = "DROP TABLE aalocations;"; 
-// Sample SQL statement to query the entire contents of a table: 
-// var thisQuery = "SELECT * FROM aalocations;";
-// Sample SQL statement to delete the entire contents of a table:
-// var thisQuery = "DELETE FROM aalocations;";
-
-
-client.query(thisQuery, (err, res) => {
-    console.log(err, res);
-    client.end();
-});
-
-// var thisQuery = "SELECT * FROM aalocations;";
+    var thisQuery = "INSERT INTO aalocations VALUES (E'" + value.oldaddress + "', " + value.lat + ", " + value.lon + ", '{" + value.adrMeta + "}',  '" + cleanTitle + "', " + value.wheelchair + ", '{" + value.meetings + "}');"; 
+    console.log(thisQuery)
+    client.query(thisQuery, (err, res) => {
+        console.log(err, res);
+        client.end();
+    });
+    setTimeout(callback, 1000); 
+}); 
--- assignment-4/week4_populateTable.js
+++ assignment-4/week4_populateTable.js
@@ -13,13 +13,18 @@ db_credentials.database = 'aaDatabase';
 db_credentials.password = process.env.AWSRDS_PW;
 db_credentials.port = 5432;
 
-// var addressesForDb = [ { address: '63 Fifth Ave, New York, NY', latLong: { lat: 40.7353041, lng: -73.99413539999999 } }, { address: '16 E 16th St, New York, NY', latLong: { lat: 40.736765, lng: -73.9919024 } }, { address: '2 W 13th St, New York, NY', latLong: { lat: 40.7353297, lng: -73.99447889999999 } } ];
-var addressesForDb = [ { address: '63 Fifth Ave, New York, NY', latLong: { lat: 40.7353041, lng: -73.99413539999999 } }, { address: '16 E 16th St, New York, NY', latLong: { lat: 40.736765, lng: -73.9919024 } }, { address: '2 W 13th St, New York, NY', latLong: { lat: 40.7353297, lng: -73.99447889999999 } } ];;
+// var addressesForDb = [{"oldaddress":"20 Cardinal Hayes Place","new address":"20 CARDINAL HAYES PL New York NY ","lat":"40.7132514","lon":"-74.002398","adrMeta":["Rectory Basement","Between Duane and Pearl St"],"title":"St Andrews Church","wheelchair":false,"meetings":["Thursdays","7:00 AM","8:00 AM","OD"]},{"oldaddress":"20 Cardinal Hayes Place","new address":"20 CARDINAL HAYES PL New York NY ","lat":"40.7132514","lon":"-74.002398","adrMeta":["Rectory Basement","Between Duane and Pearl St."],"title":"St Andrews Church","wheelchair":false,"meetings":["Tuesdays","7:00 AM","8:00 AM","Op"]},{"oldaddress":"20 Cardinal Hayes Place","new address":"20 CARDINAL HAYES PL New York NY ","lat":"40.7132514","lon":"-74.002398","adrMeta":["Enter through driveway behind Church."],"title":"St. Andrew's Church","wheelchair":false,"meetings":["Mondays","12:15 PM","1:15 PM","OD"]},{"oldaddress":"20 Cardinal Hayes Place","new address":"20 CARDINAL HAYES PL New York NY ","lat":"40.7132514","lon":"-74.002398","adrMeta":["Enter through driveway behind Church."],"title":"St. Andrew's Church","wheelchair":false,"meetings":["Wednesdays","12:15 PM","1:15 PM","Op"]},{"oldaddress":"20 Cardinal Hayes Place","new address":"20 CARDINAL HAYES PL New York NY ","lat":"40.7132514","lon":"-74.002398","adrMeta":["Enter through driveway behind Church."],"title":"St. Andrew's Church","wheelchair":false,"meetings":["Thursdays","12:15 PM","1:15 PM","S"]},{"oldaddress":"20 Cardinal Hayes Place","new address":"20 CARDINAL HAYES PL New York NY ","lat":"40.7132514","lon":"-74.002398","adrMeta":["Enter through driveway behind Church."],"title":"St. Andrew's Church","wheelchair":false,"meetings":["Fridays","12:15 PM","1:15 PM","B"]}];
+var addressesForDb = JSON.parse(fs.readFileSync('data/finalCompiledData.json'));
+console.log(addressesForDb)
 
 async.eachSeries(addressesForDb, function(value, callback) {
     const client = new Client(db_credentials);
     client.connect();
-    var thisQuery = "INSERT INTO aalocations VALUES (E'" + value.address + "', " + value.latLong.lat + ", " + value.latLong.lng + ");";
+    var cleanTitle = value.title.replace(/\'/g,"");
+    console.log(cleanTitle)
+
+    var thisQuery = "INSERT INTO aalocations VALUES (E'" + value.oldaddress + "', " + value.lat + ", " + value.lon + ", '{" + value.adrMeta + "}',  '" + cleanTitle + "', " + value.wheelchair + ", '{" + value.meetings + "}');"; 
+    console.log(thisQuery)
     client.query(thisQuery, (err, res) => {
         console.log(err, res);
         client.end();
